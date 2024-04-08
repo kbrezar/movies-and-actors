e3034117b593795ff457d07d73782c8a4efdc993
kbrezar <kristof.brezar@hotmail.com>
2024-04-08 03:35:24 +0200

    added request counting

--- src/main/java/ActorEndpoint.java
+++ src/main/java/ActorEndpoint.java
@@ -1,4 +1,7 @@
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import jakarta.ws.rs.*;
 import java.util.List;
 import jakarta.inject.*;
@@ -8,35 +11,36 @@ public class ActorEndpoint {
 
     @Inject
     ActorResource actorResource;
-
+    MeterRegistry registry = Metrics.globalRegistry;
+    @Counted(value = "counted.requests")
     @GET
     public List<Actor> getActors() {
         return actorResource.getActors();
     }
-
+    @Counted(value = "counted.requests")
     @GET
     @Path("/page/{pageNum}:{pageSize}")
     public List<Actor> getActorsPaged(@PathParam("pageNum") int pageNum, @PathParam("pageSize") int pageSize) {
         return actorResource.getActorsPaged(pageNum, pageSize);
     }
-
+    @Counted(value = "counted.requests")
     @GET
     @Path("/{id}")
     public Actor getActor(@PathParam("id") String id) {
         return actorResource.getActor(id);
     }
-
+    @Counted(value = "counted.requests")
     @PUT
     @Path("/{id}")
     public Actor putActor(@PathParam("id") String id, Actor a) {
         return actorResource.updateActor(id, a);
     }
-
+    @Counted(value = "counted.requests")
     @POST
     public Actor addActor(Actor a) {
         return actorResource.addActor(a);
     }
-
+    @Counted(value = "counted.requests")
     @DELETE
     @Path("/{id}")
     public void deleteActor(@PathParam("id") String id) {
--- src/main/java/MovieEndpoint.java
+++ src/main/java/MovieEndpoint.java
@@ -1,4 +1,7 @@
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Metrics;
 import jakarta.ws.rs.*;
 import java.util.List;
 import jakarta.inject.*;
@@ -8,39 +11,40 @@ class MovieEndpoint {
 
     @Inject
     MovieResource movieResource;
-
+    MeterRegistry registry = Metrics.globalRegistry;
+    @Counted(value = "counted.requests")
     @GET
     public List<Movie> getMovies() {
         return movieResource.getMovies();
     }
-
+    @Counted(value = "counted.requests")
     @GET
     @Path("/page/{pageNum}:{pageSize}")
     public List<Movie> getMoviesPaged(@PathParam("pageNum") int pageNum, @PathParam("pageSize") int pageSize) {
         return movieResource.getMoviesPaged(pageNum, pageSize);
     }
-
+    @Counted(value = "counted.requests")
     @GET
     @Path("/{id}")
     public Movie getMovie(@PathParam("id") String id) {
         return movieResource.getMovie(id);
     }
-
+    @Counted(value = "counted.requests")
     @GET
     @Path("/search/{query}")
     public Movie findMovie(@PathParam("query") String query) { return movieResource.searchForMovieByTitle(query); }
-
+    @Counted(value = "counted.requests")
     @PUT
     @Path("/{id}")
     public Movie putMovie(@PathParam("id") String id, Movie m) {
         return movieResource.updateMovie(id, m);
     }
-
+    @Counted(value = "counted.requests")
     @POST
     public Movie addMovie(Movie m) {
         return movieResource.addMovie(m);
     }
-
+    @Counted(value = "counted.requests")
     @DELETE
     @Path("/{id}")
     public void deleteMovie(@PathParam("id") String id) {
